9aa1e514b70f53f57ae2a822aeb223fb83d91032
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-06 13:48:23 -0700

    Update app.js and style.css
    I created the function of which will prints the todo tasks in the todo list and i also created a hover effect for buttons in CSS.

--- app.js
+++ app.js
@@ -2,33 +2,64 @@ let myInput = document.querySelector("#my-input");
 let addToDoBtn = document.querySelector("#add-to-do-btn");
 let toDoListDiv = document.querySelector(".to-do-list");
 
-const addToDo = () => {
-    let createNewDiv = document.createElement("div");
-    let createNewUl = document.createElement("ul");
-    let createNewLi = document.createElement("li");
-    let createNewDeleteBtn = document.createElement("button");
-    
-    createNewDiv.classList.add("flex-box", "mt-4");
-    createNewLi.classList.add("todo-text");
-    createNewDeleteBtn.classList.add("to-do-btn");
-    createNewDeleteBtn.innerText = `delete`;
-    createNewLi.innerText = myInput.value;
-
-    toDoListDiv.append(createNewDiv);
-    createNewDiv.append(createNewUl);
-    createNewUl.append(createNewLi);
-    createNewDiv.append(createNewDeleteBtn);
-    
-    localStorage.setItem("Todo", JSON.stringify(createNewLi.innerText));
-    myInput.value = ``;
+const getToDo = () => {
+    return JSON.parse(localStorage.getItem("Todo"));
 };
 
+let arrOfToDoList = getToDo() || [];
+
+const createElements = (text) => {
+    let createNewDiv = document.createElement("div");
+        let createNewUl = document.createElement("ul");
+        let createNewLi = document.createElement("li");
+        let createNewDeleteBtn = document.createElement("button");
+        
+        createNewDiv.classList.add("flex-box", "mt-4");
+        createNewLi.classList.add("todo-text");
+        createNewDeleteBtn.classList.add("to-do-btn");
+        createNewDeleteBtn.innerText = `delete`;
+        createNewLi.innerText = text;
+        
+        toDoListDiv.append(createNewDiv);
+        createNewDiv.append(createNewUl);
+        createNewUl.append(createNewLi);
+        createNewDiv.append(createNewDeleteBtn);
+    
+        let deleteBtn = toDoListDiv.lastElementChild.children[1];
+        deleteBtn.addEventListener("click", () => {
+            let parentOfDeleteBtn = deleteBtn.parentElement;
+            parentOfDeleteBtn.style.display = `none`;
+            localStorage.removeItem("Todo");
+            arrOfToDoList = [];
+        })
+};
+
+const addToDo = (val) => {
+    if (myInput.value !== `` && !arrOfToDoList.includes(myInput.value)) {
+        arrOfToDoList.push(val.trim());
+        arrOfToDoList = [... new Set(arrOfToDoList)];
+    
+        localStorage.setItem("Todo", JSON.stringify(arrOfToDoList));
+        
+        myInput.value = ``;
+
+        createElements(val);
+    }
+
+    else{
+        myInput.value = ``;
+    }
+};
+
+const showToDo = () => {
+    arrOfToDoList.forEach(element => {
+        createElements(element)
+    });
+};
+
+showToDo();
+
 addToDoBtn.addEventListener("click", () => {
-    addToDo();
-    let deleteBtn = toDoListDiv.lastElementChild.children[1];
-    deleteBtn.addEventListener("click" , () => {
-        let parentOfDeleteBtn = deleteBtn.parentElement;
-        parentOfDeleteBtn.style.display = `none`;
-        localStorage.removeItem("Todo");
-    })
+    addToDo(myInput.value);
 });
+console.log(arrOfToDoList);
